5ba6aecaace18d9eae1ade0ad62ff58c0f14457e
Swikriti Poudel <poudelswikriti76@gmail.com>
2025-09-18 15:09:51 +0545

    Add basic light/dark theme toggle (still needs refinement)

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
+const themeIcon = document.getElementById('theme-icon');
+const html = document.documentElement;
 
 
+themeIcon.addEventListener('click', () => {
+    const currentTheme = html.getAttribute('data-theme');
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    
+    html.setAttribute('data-theme', newTheme);
+});
